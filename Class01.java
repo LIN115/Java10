01e56a78d028915be841636a397425fcc06b3e2b
LIN115 <113489076+LIN115@users.noreply.github.com>
2022-10-23 15:49:12 +0800

    Add files via upload

---
+++ Class01.java
@@ -0,0 +1,28 @@
+package Java10;
+
+class Caaa {
+	public int num1;
+	public int num2;
+}
+
+class Cbbb extends Caaa {
+	public void set_num(int num1, int num2) {
+		System.out.println("Num1:" + num1);
+		System.out.println("Num2:" + num2);
+	}
+
+	public void show() {
+
+	}
+}
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cbbb bb = new Cbbb();
+		bb.set_num(5, 10);
+		bb.show();
+	}
+
+}
---
+++ Class03.java
@@ -0,0 +1,25 @@
+package Java10;
+
+class Caaa {
+	public int num1;
+	public int num2;
+
+	private Caaa() {
+		num1 = 1;
+		num2 = 1;
+	}
+
+	private Caaa(int a, int b) {
+		num1 = a;
+		num2 = b;
+	}
+}
+
+public class Class03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
